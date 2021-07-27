a22bf7cb387bf17bf69bbda18d9e2e39cfc1228f
lkj <kyoung_ju@naver.com>
2021-07-27 21:52:45 +0900

    bootstrap 추가

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ]/*,
+  modules: ['bootstrap-vue/nuxt'],
+  bootstrapVue: {
+    bootstrapCSS: false,
+    bootstrapVueCSS: false
+  }*/
 }
